d576f895a597cbc9f844ef3c1937c68ac16ccb00
Aminul Islam <aminul.islam.157246@gmail.com>
2026-02-01 22:37:31 +0600

    destructuring

---
+++ destructuring.js
@@ -0,0 +1,9 @@
+// object destructuring
+const device = {name : 'phone', brand : 'symphony', quantity : 9}
+const {brand} = device;
+console.log(brand)
+
+// array destructuring
+const numbers = [4,5,6,7]
+const [first, second, third]  = numbers;
+console.log(first, second)
