9bfc7ead8d7d633d6c66ed5ce56603a77c40c2f3
Aminul Islam <aminul.islam.157246@gmail.com>
2026-02-01 22:16:13 +0600

    array spreading

---
+++ spread.js
@@ -0,0 +1,12 @@
+const numbers = [4,5,6,7]
+console.log(...numbers)  // 4 5 6 7
+
+// const arrayMax = Math.max(numbers)  // NaN
+const arrayMax = Math.max(...numbers)
+console.log(arrayMax)
+
+
+
+const total = (a,b,c) => a + b + c
+const digits = [8,9,10,11]
+console.log(total(...digits))
