07e011a0c066f6c14854b16350bf731c4547ae39
Aminul Islam <aminul.islam.157246@gmail.com>
2026-02-01 21:42:54 +0600

    function

---
+++ function.js
@@ -0,0 +1,18 @@
+function add(num1, num2){
+    return num1 + num2
+}
+console.log(add(4,5))
+
+
+// function expression 
+const subtraction = function(num1, num2){
+    return num1 + num2
+}
+
+console.log(subtraction(7,5))
+
+// arrow function 
+const multiplication = (num1, num2) => {
+    return num1 * num2
+}
+console.log(multiplication(4,5))
