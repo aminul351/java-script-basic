34a3ab2b2dc8b6426223ad5a1670ca888ae35863
Aminul Islam <aminul.islam.157246@gmail.com>
2026-02-04 00:35:21 +0600

    class

---
+++ 8_class.js
@@ -0,0 +1,18 @@
+class Player {
+
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+        this.location = 'BD'
+    }
+
+    goal() {
+        console.log('this player score a goal')
+    }
+
+}
+
+const player1 = new Player('Rodri', 33)
+const player2 = new Player('Halland', 35)
+player1.goal()
+console.log(player1,player2)
